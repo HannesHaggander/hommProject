cfd8a99181cad764e15f2a4515b0971453c5a38a
HannesHaggander <h.haggander@gmail.com>
2016-10-08 14:16:05 +0200

    added ui elements to town screen

---
+++ hommproject/Assets/Scripts/Town/SelectStructure.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SelectStructure : MonoBehaviour {
+
+	public RectTransform structureMenu = null;
+
+	void OnMouseOver(){
+		if(structureMenu != null && Input.GetMouseButtonDown(0)){
+			structureMenu.gameObject.SetActive(true);
+		}
+	}
+
+	void Update(){
+		if(structureMenu != null && structureMenu.gameObject.active){
+			if(Input.GetKeyDown(KeyCode.Escape)){
+				structureMenu.gameObject.SetActive(false);
+			}
+		}
+	}
+}
